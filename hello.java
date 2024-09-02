0781ab03539742a61eef5bceef70334c1444f1e1
jino Dev <jinodev1507@gmail.com>
2024-09-02 23:14:43 +0530

    sumOfThreeNumbers

---
+++ hello.java
@@ -0,0 +1,13 @@
+class hello
+{
+    public static void main(String a[])
+    {
+        
+        int num1 = 5;
+        int num2 = 20;
+        int num3 = 67;
+        int result = num1 + num2 + num3;
+
+        System.out.println(result);
+    }
+}
