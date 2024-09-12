4729c97c272f21e51e946c11237552961466e956
jino Dev <jinodev1507@gmail.com>
2024-09-12 12:37:28 +0530

    sampleby

---
+++ samplexy.java
@@ -0,0 +1,14 @@
+public class samplexy {
+    public static void main(String [] args)
+    {
+        int x = 17;
+
+        if(x>10 && x<=20)
+        System.out.println("Hello");
+        else{
+            System.out.println("bye");
+        }
+
+    }
+    
+}
