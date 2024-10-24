51aa00638e49164f421b6e3567889afd180cccbc
jino Dev <jinodev1507@gmail.com>
2024-10-24 23:20:29 +0530

    string

---
+++ name.java
@@ -0,0 +1,8 @@
+public class name {
+    public static void main(String [] args)
+    {
+        String name = " Jino Dev";
+        System.out.println("Hello"+ name);
+    }
+    
+}
---
+++ string1.java
@@ -0,0 +1,20 @@
+public class string1 {
+    public static void main(String a[])
+{
+    StringBuffer sb = new StringBuffer("Jino");
+    sb.append("Dev");
+
+    sb.insert(7,"Java");
+    sb.setLength(30);
+
+    System.out.println(sb);
+
+
+
+
+
+
+
+
+}
+}
